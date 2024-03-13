fe5ddcbd19726b5db6fb5fd14f30bd91cf02f207
Majorika <12.12kimiyeon@gmail.com>
2024-03-13 12:12:51 +0900

    init commit
    🚵‍♀️ 잘부탁해

---
+++ README.md
@@ -0,0 +1,15 @@
+# 백준 알고리즘 이력
+
+### ✏️작성 취지
+1. 알고리즘 해결과정 및 고충 정리
+2. 파일 이력관리
+3. 빠른 디버깅, 실행 결과 도출
+
+### 💻구성
+Java JDK 8 ( corretto 1.8 )
+Server IntelliJ Local Machine -> Edit Configuration -> Build and run modify options -> Redirect Input & save console to output file  
+
+### 👨‍💻구현방식
+패키지마다 알고리즘 카테고리를 나눔
+백준 사이트에서 제시한 입력 데이터를 Input.txt에 작성 후 코딩
+올바른 값이 Output.txt에 저장되었는지 확인
---
+++ src/com/exam/baekjoon/string/Solution_11720.java
@@ -0,0 +1,26 @@
+package com.exam.baekjoon.string;
+
+import java.io.*;
+
+public class Solution_11720 {
+
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
+            int n = Integer.parseInt(br.readLine());
+            int sum = 0;
+            char[] chars = br.readLine().toCharArray();
+
+            for(int i = 0; i < n; i++){
+                //각 char 숫자를 추출하여 sum
+                sum += Character.getNumericValue(chars[i]);
+            }
+
+            bw.write(String.valueOf(sum));
+            bw.flush();
+        }catch(IOException ioe){
+            System.err.print(ioe.getMessage());
+        }
+    }
+
+}
