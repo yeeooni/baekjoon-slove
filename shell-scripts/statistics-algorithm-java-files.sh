0bf7e29b7a164cac1af361459f7759d9da891395
EUIYEON KIM <12.12kimiyeon@gmail.com>
2024-10-11 13:13:54 +0900

    Auto stash before rebase of "main" onto "0.0.2"

--- shell-scripts/statistics-algorithm-java-files.sh
+++ shell-scripts/statistics-algorithm-java-files.sh
@@ -12,13 +12,13 @@ WEEKLY_JAVA_COUNT=$(find . -type f -name "*.java" -newermt "${SEVEN_DAYS_AGO}" !
 MONTHLY_JAVA_COUNT=$(find . -type f -name "*.java" -newermt "${THIRTY_DAYS_AGO}" ! -newermt "${TODAY}" | wc -l)
 
 # 결과 출력
-echo "+====================================================+";
-echo "Java Files Created Yesterday: ${DAILY_JAVA_COUNT}"
-echo "Java Files Created Last 7 Days: ${WEEKLY_JAVA_COUNT}"
-echo "Java Files Created Last 30 Days: ${MONTHLY_JAVA_COUNT}"
-echo "+====================================================+";
+echo -e "\e[31m+====================================================+";
+echo -e "\e[32mJava Files Created Yesterday: ${DAILY_JAVA_COUNT}"
+echo -e "\e[32mJava Files Created Last 7 Days: ${WEEKLY_JAVA_COUNT}"
+echo -e "\e[32mJava Files Created Last 30 Days: ${MONTHLY_JAVA_COUNT}"
+echo -e "\e[31m+====================================================+";
 
 # GitHub Actions 의 GITHUB_OUTPUT 파일에 값을 기록
-echo "DAILY_JAVA_COUNT=${DAILY_JAVA_COUNT}" >> "${GITHUB_OUTPUT}"
-echo "WEEKLY_JAVA_COUNT=${WEEKLY_JAVA_COUNT}" >> "${GITHUB_OUTPUT}"
-echo "MONTHLY_JAVA_COUNT=${MONTHLY_JAVA_COUNT}" >> "${GITHUB_OUTPUT}"
+echo "DAILY_JAVA_COUNT=${DAILY_JAVA_COUNT}" | tee -a "${GITHUB_OUTPUT}" 1> /dev/null
+echo "WEEKLY_JAVA_COUNT=${WEEKLY_JAVA_COUNT}" | tee -a "${GITHUB_OUTPUT}" 1> /dev/null
+echo "MONTHLY_JAVA_COUNT=${MONTHLY_JAVA_COUNT}" | tee -a "${GITHUB_OUTPUT}" 1> /dev/null
